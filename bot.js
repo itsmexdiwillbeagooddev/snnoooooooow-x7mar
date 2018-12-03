3bd16435007532af5f5f5fe475a7c00411ca76db
itsmexdiwillbeagooddev <44976003+itsmexdiwillbeagooddev@users.noreply.github.com>
2018-12-03 22:57:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,24 +26,28 @@ spee={};
 
 //source
 client.on('ready', () => {
-        console.log('╔[════════════]╗')
-        console.log(' Bot Is Online')
-        console.log('╚[════════════]╝')
-        console.log('')
-        console.log('╔[════════════════════════════════════]╗');
-        console.log('')
-        console.log(' Logged in as * [ " ${client.user.username} " ] ');
-        console.log( ' Welcome Bro , UserName * [ " ${client.user.username} " ]' );
-        console.log( :  * [ " ${client.user.tag} " ]);
-        console.log('')
-        console.log('╚[════════════════════════════════════]╝')
-        console.log('')
-        console.log('╔[════════════]╗')
-        console.log('')
-        console.log( 'Have Fun - Snow Team  ')
-        console.log('')
-        console.log('╚[════════════]╝')
-        
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
 });
 
 
