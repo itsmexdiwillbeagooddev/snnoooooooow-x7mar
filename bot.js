66b0732d1bc29b365c8d2c5628ec7e8c6d906158
itsmexdiwillbeagooddev <44976003+itsmexdiwillbeagooddev@users.noreply.github.com>
2018-12-05 18:26:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,36 @@
 const Discord = require('discord.js');
-const fs = require('fs');
-const ms = require('ms');
-const canvas = require('canvas-prebuilt');
-const jimp = require('jimp');
-const ytdl = require('ytdl-core');
-const request = require('request');
-const dateFormat = require('dateformat');
-const google = require('google-it');
+const developers = ["415595760990552065","314845344313901057"]
 const db = require('quick.db');
+const client = new Discord.Client();   
+const bot = new Discord.Client();   
 const giphy = require('giphy-api')();    
 const googl = require('goo.gl');  
 const translate = require('google-translate-api');   
+const fs = require("fs"); 
+const canvas = require("canvas");
+const getYoutubeID = require('get-youtube-id'); 
+const moment = require("moment");  
 const { Client, Util } = require('discord.js');  
 const UserBlocked = new Set(); 
-const moment = require('moment');
-const pretty = require('pretty-ms'); 
-const zalgo = require('zalgolize');   
+const jimp = require('jimp');   
 const math = require('math-expression-evaluator'); 
 const stripIndents = require('common-tags').stripIndents;
 const figlet = require('figlet');
-const client = new Discord.Client();
-const prefix = '-'
-const developers = ["415595760990552065","314845344313901057"]
-ti={}  
-spee={};
+const google = require('google-it'); 
+const queue = new Map(); 
+const zalgo = require('zalgolize');   
+const fetchVideoInfo = require('youtube-info');
+const YouTube = require('simple-youtube-api');
+const ytdl = require('ytdl-core');
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+const sql = require("sqlite");
+const dateFormat = require('dateformat'); 
+const pretty = require('pretty-ms') 
+const prefix = '-';
+var table = require('table').table
+var ti={}  
+,spee={}
+,attentions={};
 
 
 
@@ -113,7 +120,6 @@ if (message.content.startsWith(adminprefix + 'setavatar')) {
 
 
 
-
 //montion bot
 client.on('message', message=> {
     if (message.author.bot) return;
@@ -129,7 +135,7 @@ client.on('message', message=> {
 //thanks
 client.on('guildCreate', guild => {
   var embed = new Discord.RichEmbed()
-  .setColor(0x5500ff)
+  .setColor("#00FFF")
   .setDescription(` :rose: **شكراً لك لإضافه البوت الى سيرفرك**`)
       guild.owner.send(embed)
 });
@@ -480,7 +486,6 @@ if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('لي�
                message.reply("**تم فتح الشات**:snowflake:")
            });
              }
- 
  
  
 });
