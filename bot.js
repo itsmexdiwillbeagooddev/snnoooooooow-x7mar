b7cf8d3e686c2a04a266fe100507a4140db92ffb
itsmexdiwillbeagooddev <44976003+itsmexdiwillbeagooddev@users.noreply.github.com>
2018-12-11 20:37:45 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1264,24 +1264,6 @@ client.on('message', message => {
   });
 
 
-//unabn all
-client.on('message', message => {
-    var prefix = "-";
-    if (message.author.bot) return;
-    if (!message.content.startsWith(prefix)) return;
- 
-    let command = message.content.split(" ")[0];
-    command = command.slice(prefix.length);
-  let args = message.content.split(" ").slice(1);
-    if (message.content.startWith(prefix + "unall")){
-if(!message.channel.guild) return;
- message.guild.members.forEach( member => {
-     
-     member.unban()
- })
-}
-});
-
 
 
 
