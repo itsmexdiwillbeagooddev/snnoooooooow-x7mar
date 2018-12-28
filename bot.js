0a5ec891063ed3612849acfc014c45ccc7d5849f
itsmexdiwillbeagooddev <44976003+itsmexdiwillbeagooddev@users.noreply.github.com>
2018-12-28 10:59:02 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1440,13 +1440,12 @@ if(message.content =='-members')
 var IzRo = new Discord.RichEmbed()
 .setThumbnail(message.author.avatarURL)
 .setFooter(message.author.username, message.author.avatarURL) 
-.setTitle('🌷| Members info')
-.addField('📗| Online',
-`${message.guild.members.filter(m=>m.presence.status == 'online').size}`)
-.addField('📕| DND',`${message.guild.members.filter(m=>m.presence.status == 'dnd').size}`)
-.addField('📙| Idle',`${message.guild.members.filter(m=>m.presence.status == 'idle').size}`)
-.addField('📓| Offline',`${message.guild.members.filter(m=>m.presence.status == 'offline').size}`)
-.addField('➡| Server Members',`${message.guild.memberCount}`)
+.setTitle(' Server Members Stats')
+.addField('📗| Online : `${message.guild.members.filter(m=>m.presence.status == 'online').size}` ')
+.addField('📕| DND : `${message.guild.members.filter(m=>m.presence.status == 'dnd').size}` ')
+.addField('📙| Idle : `${message.guild.members.filter(m=>m.presence.status == 'idle').size}` ')
+.addField('📓| Offline : `${message.guild.members.filter(m=>m.presence.status == 'offline').size}` ')
+.addField('➡| Server Members : `${message.guild.memberCount}` ')
 message.channel.send(IzRo);
 
 });
